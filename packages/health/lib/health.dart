3bc508899e95d347fbc7167e07a64c618300aa63
lichaochao <77885670+Jasonli-cc@users.noreply.github.com>
2024-10-06 13:31:35 +0800

    导包

--- packages/health/lib/health.dart
+++ packages/health/lib/health.dart
@@ -5,7 +5,6 @@ import 'dart:collection';
 import 'dart:io' show Platform;
 
 import 'package:carp_serializable/carp_serializable.dart';
-import 'package:health/src/hrv_data.dart';
 import 'package:intl/intl.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:device_info_plus/device_info_plus.dart';
@@ -13,6 +12,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 part 'src/workout_route_data.dart';
+part 'src/hrv_data.dart';
 part 'src/health_activity_summary.dart';
 part 'src/heath_data_types.dart';
 part 'src/functions.dart';
